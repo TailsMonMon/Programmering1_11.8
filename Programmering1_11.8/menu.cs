021f6bac2eecaae430c0636715ba02b77a872bbd
Monica Björk <m.bjork95@gmail.com>
2018-04-22 15:54:27 +0200

    Add project files.

---
+++ Programmering1_11.8/menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_11._8 {
+    class menu {
+
+        ////      METHOD TO CALCULATE SALES TAX
+        //static string MyMoms(double price, double moms) {
+        //    double priceWithMoms = Math.Round(((moms / 100 + 1) * price));
+        //    // moms/100+1 makes the precent into decimals. then multiplices with the price to get the result.
+        //    string resultPriceMoms = "Priset med moms blir " + priceWithMoms;
+        //    return resultPriceMoms;
+        //}
+
+
+        ////      ADDERA()
+        //static int Addera(int num1, int num2) {
+        //    int summa = num1 + num2;
+        //    return summa;
+        //}
+
+
+        ////      SKRIVA UT TALFÖLJD
+        //int num = 1;
+
+        //    do {
+        //        Console.WriteLine(num++);
+        //    } while(num <= 20);
+        //Console.ReadLine();
+
+
+        ////      KONTROLLERA VÄDERET
+        //Console.Write("Är det fint väder idag? j/n: ");
+        //    string answer = Console.ReadLine();
+
+        //    if(answer == "j") {
+        //        Console.WriteLine("Vi går på picknick!");
+        //    }
+        //    else if(answer == "n") {
+        //        Console.WriteLine("Vi stannar inne och läser en bok");
+        //    }
+        //    else {
+        //        Console.WriteLine("Jag förstår inte");
+        //    }
+        //Console.ReadLine();
+
+    }
